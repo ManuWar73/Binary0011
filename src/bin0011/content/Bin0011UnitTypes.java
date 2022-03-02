6e166287d7911e9b081a5dee82aff1af6d497fbb
1237 <92003875+12three7@users.noreply.github.com>
2022-03-02 18:18:34 +0800

    Update Bin0011UnitTypes.java

--- src/bin0011/content/Bin0011UnitTypes.java
+++ src/bin0011/content/Bin0011UnitTypes.java
@@ -1,21 +1,18 @@
 package bin0011.content;
 
-import arc.graphics.*;
-import arc.graphics.g2d.*;
-import arc.math.*;
+package abyss.content;
+
+import arc.graphics.Color;
 import arc.struct.*;
-import mindustry.ai.types.*;
-import mindustry.ctype.*;
-import mindustry.entities.*;
-import mindustry.entities.abilities.*;
-import mindustry.entities.bullet.*;
-import mindustry.entities.effect.*;
-import mindustry.gen.*;
 import mindustry.graphics.*;
-import mindustry.type.*;
-import mindustry.type.ammo.*;
-import mindustry.type.weapons.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.abilities.*;
 import mindustry.world.meta.*;
+import mindustry.gen.*;
+import mindustry.ctype.ContentList;
+import mindustry.type.*;
 
 import static arc.graphics.g2d.Draw.*;
 import static arc.graphics.g2d.Lines.*;
@@ -25,12 +22,11 @@ import bin0011.content.*;
 
 public class Bin0011UnitTypes implements ContentList{
 	public static UnitType
-		guram;
-        // guram class units enters mech class, you can change it
-	// b e n t a r. kalo gak salah insectoid punya class sendiri (bukan mech) - 1237
-	public static @EntityDef({Unitc.class, Legsc.class}) UnitType guram;
+		// malar
+	 	guram;
 	@Override
 	public void load() {
+		// malar start
 		guram = new UnitType("guram"){{
 			localizedName = "Guram";
 	 	 	speed = 0.67f;
@@ -48,5 +44,6 @@ public class Bin0011UnitTypes implements ContentList{
             	 	visualElevation = 0.3f;
 	 	 	groundLayer = Layer.legUnit - 1f;
 		}};
+		// malar end
 	}
 }
