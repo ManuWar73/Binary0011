7184c88a009296b72f852af12aa9ab6deafc6bef
1237 <92003875+12three7@users.noreply.github.com>
2022-03-02 17:56:25 +0800

    Update Bin0011UnitTypes.java

--- src/bin0011/content/Bin0011UnitTypes.java
+++ src/bin0011/content/Bin0011UnitTypes.java
@@ -22,6 +22,7 @@ import static arc.graphics.g2d.Lines.*;
 import static arc.math.Angles.*;
 import static mindustry.Vars.*;
 import bin0011.content.*;
+import mindustry.content.*
 
 public class Bin0011UnitTypes implements ContentList{
 	public static UnitType
