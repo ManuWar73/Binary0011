5fa92477177bfc509e67482772ebda66859c0235
1237 <92003875+12three7@users.noreply.github.com>
2022-03-19 16:04:06 +0800

    Create blocks.js

---
+++ assets/scripts/blocks.js
@@ -0,0 +1,11 @@
+const bt0000 = extend(ItemTurret, "bt0000", {
+	size: 1,
+	health: 200,
+	reloadTime: 60,
+	spread: 4,
+	ammo: (Items.copper, Bullets.standardCopper, Items.graphite, Bullets.standardDense, Items.pyratite, Bullets.standardIncendiary, Items.silicon, Bullets.standardHoming),
+	range: 192,
+	recoilAmount: 0.3,
+	shootCone: 15,
+	rotateSpeed: 10
+});
