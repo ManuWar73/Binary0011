3010a20642d731ad8215458bd23d55ec56806520
1237 <92003875+12three7@users.noreply.github.com>
2022-03-26 16:52:23 +0800

    Update B11Items.java

--- src/b11/content/B11Items.java
+++ src/b11/content/B11Items.java
@@ -3,3 +3,16 @@ package b11.content;
 import arc.graphics.Color;
 import mindustry.ctype.ContentList;
 import mindustry.type.*;
+
+public class B11Items implements ContentList{
+	public static Item 
+	item00, item01, item10, item11;
+	@Override
+	public void load(){
+		item00 = new Item("item00"){{
+			cost = 0.1f;
+			hardness = 0;
+		}};
+		item01 = item00;
+	}
+}
