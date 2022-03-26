5b2dd6dff135cf740eeb95ecc851c0ac4dc47434
1237 <92003875+12three7@users.noreply.github.com>
2022-03-27 07:55:46 +0800

    Update B11Bullets.java

--- src/b11/content/B11Bullets.java
+++ src/b11/content/B11Bullets.java
@@ -9,7 +9,8 @@ import b11.content.*;
 
 public class B11Bullets implements ContentList {
 	public static BulletType
-	basicbullet1, basicbullet2;
+	basicbullet1, basicbullet2, basicbullet3, basicbullet4
+	;
 	@Override
 	public void load(){
 		basicbullet1 = new BasicBulletType(2.5f, 14){{
@@ -25,6 +26,24 @@ public class B11Bullets implements ContentList {
 			shootEffect = Fx.shootSmall;
 			smokeEffect = Fx.shootSmallSmoke;
 			ammoMultiplier = 2;
+		}};
+		basicbullet3 = new BasicBulletType(5f, 28){{
+			width = height = 7.5f;
+			lifetime = 60 * 3f;
+			shootEffect = Fx.shootSmall;
+			smokeEffect = Fx.shootSmallSmoke;
+			ammoMultiplier = 3;
+			homingPower = 100f;
+			homingRange = 45f * 8f;
+		}};
+		basicbullet4 = new BasicBulletType(5f, 56){{
+			width = height = 9f;
+			lifetime = 60 * 3f;
+			shootEffect = Fx.shootSmall;
+			smokeEffect = Fx.shootSmallSmoke;
+			ammoMultiplier = 3;
+			homingPower = 100f;
+			homingRange = 45f * 8f;
 		}};
 	}
 }
