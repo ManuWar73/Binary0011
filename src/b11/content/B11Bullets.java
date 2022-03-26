0079dca9ddc8d444e3e0e8f8e045d62c3d4e8ccc
1237 <92003875+12three7@users.noreply.github.com>
2022-03-27 07:33:35 +0800

    Update B11Bullets.java

--- src/b11/content/B11Bullets.java
+++ src/b11/content/B11Bullets.java
@@ -9,13 +9,19 @@ import b11.content.*;
 
 public class B11Bullets implements ContentList {
 	public static BulletType
-
-	basicbullet1;
+	basicbullet1, basicbullet2;
 	@Override
 	public void load(){
 		basicbullet1 = new BasicBulletType(2.5f, 14){{
 			width = height = 7.5f;
-			lifetime = 60f;
+			lifetime = 60 * 2f;
+			shootEffect = Fx.shootSmall;
+			smokeEffect = Fx.shootSmallSmoke;
+			ammoMultiplier = 2;
+		}};
+		basicbullet2 = new BasicBulletType(3.5f, 21){{
+			width = height = 7.5f;
+			lifetime = 60 * 2f;
 			shootEffect = Fx.shootSmall;
 			smokeEffect = Fx.shootSmallSmoke;
 			ammoMultiplier = 2;
