e46dd803f86ae0e7193cbd59f70df10787ace187
1237 <92003875+12three7@users.noreply.github.com>
2022-03-26 18:38:41 +0800

    Update B11Bullets.java

--- src/b11/content/B11Bullets.java
+++ src/b11/content/B11Bullets.java
@@ -1,1 +1,26 @@
-//
+package b11.content;
+
+import arc.graphics.Color;
+import mindustry.content.*;
+import mindustry.ctype.ContentList;
+import mindustry.entities.bullet.*;
+import b11.*;
+import b11.content.*;
+
+public class B11Bullets implements ContentList {
+	public static BulletType
+
+	basicbullet1;
+	@Override
+	public void load(){
+		basicbullet1 = new BasicBulletType(2.5f, 14){{
+			width = height = 7.5f;
+			lifetime = 60f;
+			shootEffect = Fx.shootSmall;
+			smokeEffect = Fx.shootSmallSmoke;
+			ammoMultiplier = 2;
+		}};
+	}
+}
+
+
