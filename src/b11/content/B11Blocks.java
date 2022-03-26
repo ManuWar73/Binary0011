95f05def108d53001f1e3af5e7ffe7f6b0ec8317
1237 <92003875+12three7@users.noreply.github.com>
2022-03-26 16:36:58 +0800

    Create B11Blocks.java

---
+++ src/b11/content/B11Blocks.java
@@ -0,0 +1,33 @@
+package b11.content;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.ctype.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.Pal;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.meta.*;
+import mindustry.world.draw.*;
+
+import b11.content.*;
+
+import static mindustry.type.ItemStack.*;
+import static mindustry.type.ItemStack.with;
+
