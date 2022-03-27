652b6cbd17c65d401e621d20d89a7eb0a1f7cd76
1237 <92003875+12three7@users.noreply.github.com>
2022-03-27 08:59:33 +0800

    m o r e b u l l e t c o n s t a n t s

--- src/b11/content/B11Bullets.java
+++ src/b11/content/B11Bullets.java
@@ -9,10 +9,14 @@ import b11.content.*;
 
 public class B11Bullets implements ContentList {
 	public static BulletType
-	basicbullet1, basicbullet2, basicbullet3, basicbullet4
+	// basic bullets
+	basicbullet1, basicbullet2, basicbullet3, basicbullet4,
+	// pawer bullets
+	lightningbullet1, laserbullet1, laserbullet2
 	;
 	@Override
 	public void load(){
+		// basic start
 		basicbullet1 = new BasicBulletType(2.5f, 14){{
 			width = height = 7.5f;
 			lifetime = 60 * 2f;
@@ -45,6 +49,33 @@ public class B11Bullets implements ContentList {
 			homingPower = 100f;
 			homingRange = 45f * 8f;
 		}};
+		// basic end
+		// pawer start
+		lightningbullet1 = new LightningBulletType(){{
+			damage = 35;
+			lightningLength = 25;
+			collidesAir = true;
+			ammoMultiplier = 1f;
+		}};
+		laserbullet1 = new LaserBulletType(154){{
+			colors = new Color[]{Color.valueOf("959595"), Color.valueOf("BABABA"), Color.white};
+			hitSize = 4;
+			lifetime = 16f;
+			drawSize = 400f;
+			collidesAir = true;
+			length = 192f;
+			ammoMultiplier = 1f;
+		}};
+		laserbullet2 = new LaserBulletType(210){{
+			colors = new Color[]{Color.valueOf("a0a0a0"), Color.valueOf("dfdfdf"), Color.white};
+			hitSize = 4;
+			lifetime = 8f;
+			drawSize = 400f;
+			collidesAir = true;
+			length = 288f;
+			ammoMultiplier = 1f;
+		}};
+		// pawer end
 	}
 }
 
