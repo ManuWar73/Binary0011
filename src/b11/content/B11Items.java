e522398e26393a6e88003049b55846273f00a6ae
1237 <92003875+12three7@users.noreply.github.com>
2022-03-29 11:50:26 +0800

    ok

--- src/b11/content/B11Items.java
+++ src/b11/content/B11Items.java
@@ -9,20 +9,24 @@ public class B11Items implements ContentList{
 	item00, item01, item10, item11;
 	@Override
 	public void load(){
-		item00 = new Item("item00"){{
+		item00 = new Item("item00", Color.valueOf("54555e")){{
 			cost = 0.1f;
+			localizedName = "item00";
 			hardness = 0;
 		}};
-		item01 = new Item("item01"){{
+		item01 = new Item("item01", Color.valueOf("b2bbc2")){{
 			cost = 0.1f;
+			localizedName = "item01";
 			hardness = 0;
 		}};
-		item10 = new Item("item10"){{
+		item10 = new Item("item10", Color.valueOf("54555e")){{
 			cost = 0.2f;
+			localizedName = "item10";
 			hardness = 0;
 		}};
-		item11 = new Item("item11"){{
+		item11 = new Item("item11", Color.valueOf("b2bbc2")){{
 			cost = 0.2f;
+			localizedName = "item11";
 			hardness = 0;
 			charge = 0.05f;
 		}};
