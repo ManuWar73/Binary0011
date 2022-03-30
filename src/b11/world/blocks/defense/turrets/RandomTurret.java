9747b6f2b88986b13e6f42b2c7b22bb8fa68ff2a
1237 <92003875+12three7@users.noreply.github.com>
2022-03-30 15:57:42 +0800

    Update RandomTurret.java

--- src/b11/world/blocks/defense/turrets/RandomTurret.java
+++ src/b11/world/blocks/defense/turrets/RandomTurret.java
@@ -42,11 +42,6 @@ public class RandomTurret extends Turret{
     @Override
     public void init(){
         @Override
-        public boolean valid(Building entity){
-            return entity instanceof ItemTurretBuild it && !it.ammo.isEmpty();
-        }
-
-        @Override
         public void display(Stats stats){
             //don't display
         }
