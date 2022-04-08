57302f76fc985f53069479c6edbc8c3142cb78f7
1237 <92003875+12three7@users.noreply.github.com>
2022-04-08 20:06:49 +0800

    Update B11Blocks.java

--- src/b11/content/B11Blocks.java
+++ src/b11/content/B11Blocks.java
@@ -1,1 +1,40 @@
-//todo
+/*package b11.content;
+import arc.graphics.*;
+import arc.math.*;
+import arc.struct.*;
+import mindustry.*;
+import mindustry.ctype.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.payloads.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+import static mindustry.type.ItemStack.*;
+public class B11Blocks implements ContentList{
+	public static Block
+	e0000, e0001;
+	@Override
+	public void load(){
+		
+	}
+}
+
