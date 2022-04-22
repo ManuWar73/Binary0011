f90e3822ea7bb640c6337e1df99dbc2744e8991d
1237 <92003875+12three7@users.noreply.github.com>
2022-04-22 22:03:50 +0800

    FIX

--- src/b11/world/blocks/defense/BulletWall.java
+++ src/b11/world/blocks/defense/BulletWall.java
@@ -11,20 +11,20 @@ public class BulletWall extends Wall {
 		super(name);
 		solid = destructible = true;
 	 	group = BlockGroup.walls;
-	 	buildCostMultiplier = 3f; // ini wall mahal, naikin cost multiplier jadi > 3 bisa bikin wall ini kemahalan wkwkw
+	 	buildCostMultiplier = 3f;
 	 	canOverdrive = drawDisabled = false;
 	}
 
 	@Override
 	public void setStats() {
 		super.setStats();
-		stats.add(Stat.bullet, bullet.damage);
+		stats.add(Stat.bullet, bullet);
 		stats.add(Stat.range, bullet.lifetime * bullet.speed/8, StatUnit.blocks);
 	}
 	public class BulletWallBuild extends WallBuild {
 		@Override
 		public void onDestroyed() {
-		 	for(int no = 0;no <= shots;no++){
+		 	for(int no = 0;no <= shots * 5;no++){
 				 bullet.create(this, this.team, x, y, Mathf.random(0, 4) * 90f);
 				 bullet.create(this, this.team, x, y, Mathf.random(0, 4) * 90f);
 			};
