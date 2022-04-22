2dee30265c328a8545474032ba6b320bf90af388
1237 <92003875+12three7@users.noreply.github.com>
2022-04-23 05:01:56 +0800

    Update BulletWall.java

--- src/b11/world/blocks/defense/BulletWall.java
+++ src/b11/world/blocks/defense/BulletWall.java
@@ -1,13 +1,13 @@
 package b11.world.blocks.defense;
 /* arc */ import arc.Core;import arc.graphics.*;import arc.graphics.g2d.*; import arc.math.*;
 /* mindus */ import mindustry.content.*; import mindustry.world.blocks.defense.*;import mindustry.entities.bullet.*;import mindustry.game.EventType.*;import mindustry.gen.*;import mindustry.graphics.*;import mindustry.type.*;import mindustry.ui.*;import mindustry.world.consumers.*;import mindustry.world.meta.*;
-
-/** a wall that ""shoots"" bullets when destroyed. in short, it's a hybrid wall-turret. */
+/*
+a wall that ""shoots"" bullets when destroyed. a hybrid wall-turret. 
+*/
 public class BulletWall extends Wall {
 	public BulletType bullet = Bullets.standardCopper;
 	public int health = 0;
 	public int shots = 3;
-	//public void bulletrange(int lt, int s){this.lt = lt; this.s = s;bullet.lifetime = this.lt; bullet.speed = this.s;}
 	public BulletWall(String name) {
 		super(name);
 		solid = destructible = true;
