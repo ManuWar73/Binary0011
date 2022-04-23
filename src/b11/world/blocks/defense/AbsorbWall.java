3a3ee11f08566bdbf6f59f910b3a3cc246544d77
1237 <92003875+12three7@users.noreply.github.com>
2022-04-23 11:59:18 +0800

    hm

--- src/b11/world/blocks/defense/AbsorbWall.java
+++ src/b11/world/blocks/defense/AbsorbWall.java
@@ -4,6 +4,7 @@ import arc.Core;import arc.graphics.*;import arc.graphics.g2d.*; import arc.math
 import mindustry.content.*; import mindustry.world.blocks.defense.*;import mindustry.entities.bullet.*;import mindustry.game.EventType.*;import mindustry.gen.*;import mindustry.graphics.*;import mindustry.type.*;import mindustry.ui.*;import mindustry.world.consumers.*;import mindustry.world.meta.*;
 public class AbsorbWall extends Wall {
 	public int health = 100;
+	public int heal = 50;
 	public AbsorbWall(String name) {
 		super(name);
 		solid = destructible = true;
@@ -25,10 +26,11 @@ public class AbsorbWall extends Wall {
 		@Override
 		public boolean collision(Bullet bullet){
 			super.collision(bullet);
-			health -= bullet.damage;
 			damagecounter += 2;
+			health -= bullet.damage;
 			if(damagecounter == 100){
-	 	 		 health += 99999999;
+	 	 		health += heal;
+				damagecounter = 0;
 			};
 			return true;
 		}
