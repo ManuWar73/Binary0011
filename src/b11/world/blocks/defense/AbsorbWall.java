942fe3bf646e8ccad1c032696c4abaa093f681e2
1237 <92003875+12three7@users.noreply.github.com>
2022-04-23 10:27:18 +0800

    kayak nya ada yang salah

--- src/b11/world/blocks/defense/AbsorbWall.java
+++ src/b11/world/blocks/defense/AbsorbWall.java
@@ -1,9 +1,9 @@
 /** a wall that can absorb enemy bullets and heals itself. hybrid mender-wall. */
 package b11.world.blocks.defense;
 import arc.Core;import arc.graphics.*;import arc.graphics.g2d.*; import arc.math.*;
-import mindustry.content.*; import mindustry.world.blocks.defense.*;import mindustry.entities.bullet.*;import mindustry.game.EventType.*;import mindustry.gen.*;import mindustry.graphics.*;import mindustry.type.*;import mindustry.ui.*;import mindustry.world.consumers.*;import mindustry.world.meta.*;
+import mindustry.content.*; import mindustry.world.blocks.defense.*;import mindustry.entities.bullet.*;import mindustry.game.EventType.*;import mindustry.gen.*;import mindustry.graphics.*;import mindustry.type.*;import mindustry.ui.*;import mindustry.world.consumers.*;import mindustry.world.meta.*; import mindustry.comp.*;
 public class AbsorbWall extends Wall {
-	public int health = 0;
+	public int health = 100;
 	public AbsorbWall(String name) {
 		super(name);
 		solid = destructible = true;
@@ -11,13 +11,16 @@ public class AbsorbWall extends Wall {
 	 	buildCostMultiplier = 3f;
 	 	canOverdrive = drawDisabled = false;
 	}
-	/*
 	@Override
 	public void setStats() {
-		super.setStats();
-		
+		super.setStats();	
 	}
 	public class AbsorbWallBuild extends WallBuild {
+		int health2 = health; // bejir
+		int damagecounter = 0;
+		public boolean damaged(){
+			damagecounter += 2;
+			if(damagecounter == 100){health += health2};
 		}
-	}*/
+	}
 }
