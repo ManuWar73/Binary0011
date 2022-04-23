56329bf92053a9d49e5e6b674d34a72799e5545c
1237 <92003875+12three7@users.noreply.github.com>
2022-04-23 10:34:36 +0800

    Update AbsorbWall.java

--- src/b11/world/blocks/defense/AbsorbWall.java
+++ src/b11/world/blocks/defense/AbsorbWall.java
@@ -1,7 +1,7 @@
 /** a wall that can absorb enemy bullets and heals itself. hybrid mender-wall. */
 package b11.world.blocks.defense;
 import arc.Core;import arc.graphics.*;import arc.graphics.g2d.*; import arc.math.*;
-import mindustry.content.*; import mindustry.world.blocks.defense.*;import mindustry.entities.bullet.*;import mindustry.game.EventType.*;import mindustry.gen.*;import mindustry.graphics.*;import mindustry.type.*;import mindustry.ui.*;import mindustry.world.consumers.*;import mindustry.world.meta.*; import mindustry.comp.*;
+import mindustry.content.*; import mindustry.world.blocks.defense.*;import mindustry.entities.bullet.*;import mindustry.game.EventType.*;import mindustry.gen.*;import mindustry.graphics.*;import mindustry.type.*;import mindustry.ui.*;import mindustry.world.consumers.*;import mindustry.world.meta.*;
 public class AbsorbWall extends Wall {
 	public int health = 100;
 	public AbsorbWall(String name) {
@@ -16,7 +16,7 @@ public class AbsorbWall extends Wall {
 		super.setStats();	
 	}
 	public class AbsorbWallBuild extends WallBuild {
-		int health2 = health; // bejir
+		int health2 = (float) health; // aneh
 		int damagecounter = 0;
 		public boolean damaged(){
 			damagecounter += 2;
