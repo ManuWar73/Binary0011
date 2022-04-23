60f0da92a743cd4f9e8d35caf433f57e520a145d
1237 <92003875+12three7@users.noreply.github.com>
2022-04-23 13:29:06 +0800

    VARIABLE SHADOWING GAADA AKHLAK

--- src/b11/world/blocks/defense/AbsorbWall.java
+++ src/b11/world/blocks/defense/AbsorbWall.java
@@ -3,11 +3,9 @@ package b11.world.blocks.defense;
 import arc.Core;import arc.graphics.*;import arc.graphics.g2d.*; import arc.math.*;
 import mindustry.content.*; import mindustry.world.blocks.defense.*;import mindustry.entities.bullet.*;import mindustry.game.EventType.*;import mindustry.gen.*;import mindustry.graphics.*;import mindustry.type.*;import mindustry.ui.*;import mindustry.world.consumers.*;import mindustry.world.meta.*;
 public class AbsorbWall extends Wall {
-	public int health = 500;
 	public int heal = 1000;
 	public AbsorbWall(String name) {
 		super(name);
-		health = 1237;
 		solid = destructible = true;
 	 	group = BlockGroup.walls;
 	 	buildCostMultiplier = 3f;
