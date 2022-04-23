ffebc023741c56fbd555a0e1d496ae060b1fc9a0
1237 <92003875+12three7@users.noreply.github.com>
2022-04-23 19:31:26 +0800

    Update AbsorbWall.java

--- src/b11/world/blocks/defense/AbsorbWall.java
+++ src/b11/world/blocks/defense/AbsorbWall.java
@@ -3,7 +3,7 @@ package b11.world.blocks.defense;
 import arc.Core;import arc.graphics.*;import arc.graphics.g2d.*; import arc.math.*;
 import mindustry.content.*; import mindustry.world.blocks.defense.*;import mindustry.entities.bullet.*;import mindustry.game.EventType.*;import mindustry.gen.*;import mindustry.graphics.*;import mindustry.type.*;import mindustry.ui.*;import mindustry.world.consumers.*;import mindustry.world.meta.*;
 public class AbsorbWall extends Wall {
-	public int heal = 1000;
+	public int heal = 500;
 	public AbsorbWall(String name) {
 		super(name);
 		solid = destructible = true;
@@ -11,30 +11,34 @@ public class AbsorbWall extends Wall {
 	 	buildCostMultiplier = 3f;
 	 	canOverdrive = drawDisabled = false;
 	}
+
+	int damagecounter = 0;
+	int i = 2;
+
 	@Override
 	public void setStats() {
-		super.setStats();	
+		super.setStats();
+		stats.add(Stat.repairTime, (100f / heal * i / 60f), StatUnit.seconds);
 	}
 	@Override
 	public void setBars(){
 		super.setBars();
-		bars.add("Absorption",
+		bars.add("Damage Counter",
                         (AbsorbWallBuild entity) -> new Bar(
-                                () -> "Absorption",
-                                () -> Color.yellow,
+                                () -> "Damage Counter",
+                                () -> Pal.lightOrange,
                                 () -> entity.damagecounter
                         )
                 );
 	}
 	public class AbsorbWallBuild extends WallBuild {
-		int damagecounter = 0;
 		@Override
 		public boolean collision (Bullet bullet){
 			if(damagecounter == 100){
 	 	 		health += heal;
 				damagecounter = 0;
 			}else{
-				damagecounter += 2;
+				damagecounter += i;
 				health -= bullet.damage;
 			}
 			return true;
