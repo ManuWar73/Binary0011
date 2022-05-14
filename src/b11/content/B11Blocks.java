f9b9b076314dd4acd1c9df2c7c1eed5c9e645290
1237 <92003875+12three7@users.noreply.github.com>
2022-05-14 20:42:08 +0800

    Update B11Blocks.java

--- src/b11/content/B11Blocks.java
+++ src/b11/content/B11Blocks.java
@@ -48,10 +48,5 @@ public class B11Blocks {
 			health = 10;
 			shots = 8;
 		}};
-		test3 = new ManualPowerTurret("manual-power-turret"){{
-			requirements(Category.defense, with(Items.copper, 50000));
-			size = 2;
-			health = 10;
-		}};
 	}
 }
