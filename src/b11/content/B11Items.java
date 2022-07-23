e3adad11a7d236ce8fcc477df437f183f4a50bc4
1237 <92003875+12three7@users.noreply.github.com>
2022-07-24 07:16:09 +0800

    Update B11Items.java

--- src/b11/content/B11Items.java
+++ src/b11/content/B11Items.java
@@ -4,10 +4,10 @@ import arc.graphics.*;
 import arc.struct.*;
 import mindustry.type.*;
 
-public class B11Items{
+public class B11Items implements ContentList{
 	public static Item
 	bronze;
-	public static void load(){
+	public void load(){
 		bronze = new Item("bronze", Color.valueOf("d9806c")){{
 			cost = 1.5f;
 			hardness = 4;
